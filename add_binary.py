5feda9cd4fcb55f291c711774cac9ebaf2dfaf31
Hashim Jacobs <hjacobs@mail.sfsu.edu>
2021-11-15 00:33:51 -0500

    addBinary

--- add_binary.py
+++ add_binary.py
@@ -8,6 +8,8 @@ Output: "100"
 Example 2:
 Input: a = "1010", b = "1011"
 Output: "10101"
+>>> add_binary("1010", "1011")
+'10101'
 
 
 Constraints:
@@ -15,8 +17,17 @@ Constraints:
 1 <= a.length, b.length <= 104
 a and b consist only of '0' or '1' characters.
 Each string does not contain leading zeros except for the zero itself.
+
+Algorithm:
+compare each digit from back to front.
+if a[i] and b[i] = 0 a[i] + b[i] = 0
+else if a[i] is 1 and b[i] is 0, and vice versa, then a[i] + b[i] =
 """
+
 
 # TODO: come up with questions, edge cases, test cases, algorithm, implementation
 def add_binary(a: str, b: str) -> str:
-    pass
+    return f'{bin(int(a, 2) + int(b, 2))[2:]}'
+
+
+add_binary('1010', '1011')
